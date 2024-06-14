222b5192faaa46f90c3f89903e18dd1cfdbd56a5
junseinagao <junsei.nagao@gmail.com>
2024-06-14 19:23:01 +0900

    Revert "add using prisma/cleint example"
    This reverts commit 6f8ea61ece464b220ec496e025ec72b94a7deb59.

--- prisma/migrations/20240614044921_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "CotentMaster" (
-    "id" SERIAL NOT NULL,
-    "title" TEXT NOT NULL,
-    "content" TEXT NOT NULL,
-
-    CONSTRAINT "CotentMaster_pkey" PRIMARY KEY ("id")
-);
--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,16 +1,8 @@
 import { getDateString, getRandomUUID } from "../utils";
-import { PrismaClient } from "@prisma/client";
 
 export const dynamic = "force-dynamic";
 
-const getContentFromDB = async () => {
-  const prisma = new PrismaClient();
-  const constants = await prisma.cotentMaster.findMany();
-  return constants;
-};
-
-export default async function Page() {
-  const contents = await getContentFromDB();
+export default function Page() {
   return (
     <main className="content">
       <h1 className="heading">A server generated page!</h1>
@@ -23,17 +15,6 @@ export default async function Page() {
         <article className="card">
           <p>UUID</p>
           <h2>{getRandomUUID()}</h2>
-        </article>
-        <article className="card">
-          <p>Contetents From PrismaClient</p>
-          <div>
-            {contents.map((content) => (
-              <div key={content.id}>
-                <h2>{content.title}</h2>
-                <p>{content.content}</p>
-              </div>
-            ))}
-          </div>
         </article>
       </section>
     </main>
