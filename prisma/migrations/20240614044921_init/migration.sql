6f8ea61ece464b220ec496e025ec72b94a7deb59
junseinagao <junsei.nagao@gmail.com>
2024-06-14 13:58:55 +0900

    add using prisma/cleint example

---
+++ prisma/migrations/20240614044921_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "CotentMaster" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "CotentMaster_pkey" PRIMARY KEY ("id")
+);
--- src/app/ssr/page.tsx
+++ src/app/ssr/page.tsx
@@ -1,8 +1,16 @@
 import { getDateString, getRandomUUID } from "../utils";
+import { PrismaClient } from "@prisma/client";
 
 export const dynamic = "force-dynamic";
 
-export default function Page() {
+const getContentFromDB = async () => {
+  const prisma = new PrismaClient();
+  const constants = await prisma.cotentMaster.findMany();
+  return constants;
+};
+
+export default async function Page() {
+  const contents = await getContentFromDB();
   return (
     <main className="content">
       <h1 className="heading">A server generated page!</h1>
@@ -15,6 +23,17 @@ export default function Page() {
         <article className="card">
           <p>UUID</p>
           <h2>{getRandomUUID()}</h2>
+        </article>
+        <article className="card">
+          <p>Contetents From PrismaClient</p>
+          <div>
+            {contents.map((content) => (
+              <div key={content.id}>
+                <h2>{content.title}</h2>
+                <p>{content.content}</p>
+              </div>
+            ))}
+          </div>
         </article>
       </section>
     </main>
